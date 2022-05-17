4b186fa05accd7fe9c0fc7b6800d8f9804cb5101
CARVAJAL\miccarurb <michael.cardenas@carvajal.com>
2022-05-16 22:45:00 -0500

    PaymentMethod Enum - Client CRUD

--- Lab2/src/supermarket/controller/ClientController.java
+++ Lab2/src/supermarket/controller/ClientController.java
@@ -6,7 +6,9 @@
 package supermarket.controller;
 
 import java.util.HashSet;
+import java.util.Optional;
 import java.util.Set;
+import static java.util.function.Predicate.isEqual;
 import supermarket.model.Client;
 
 /**
@@ -14,15 +16,57 @@ import supermarket.model.Client;
  * @author miccarurb
  */
 public class ClientController {
+
     Set<Client> clients;
 
     public ClientController() {
         clients = new HashSet();
     }
-    
-    public void addClient(Client client){
+
+    public Client addClient(Client client) {
         clients.add(client);
+        return client;
+    }
+
+    /**
+     *
+     * @param id
+     * @return
+     */
+    public Optional<Client> findClientById(String id) {
+        return clients.stream().filter(c -> c.getIdentification().equals(id)).findAny();
     }
     
+    public Optional<Client> findClient(Client client) {
+        return clients.stream().filter(isEqual(client)).findAny();
+    }
     
+    public Client updateClient(Client client){
+        // Java funciona por referencias al objecto real, por lo que el uso de un metodo setter en cualquier otra clase
+        // va a modificar el objeto en todas las variables del contexto que lo referencien.
+        return client;
+    }
+
+    public Client createOrUpdateClient(Client client) {
+        Optional<Client> clientFound = findClientById(client.getIdentification());
+        if (clientFound.isPresent()) {
+            return updateClient(clientFound.get());
+        }
+        return addClient(client);
+    }
+
+    public boolean removeClient(Client client){
+        if (clients.contains(client)) {
+            clients.remove(client);
+        }
+        return true;
+    }
+    
+    public boolean removeClientById(String id){
+        Optional<Client> clientFound = findClientById(id);
+        if (clientFound.isPresent()) {
+            removeClient(clientFound.get());
+        }
+        return true;
+    }
 }
---
+++ Lab2/src/supermarket/enums/PaymentMethod.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package supermarket.enums;
+
+/**
+ *
+ * @author miccarurb
+ */
+public enum PaymentMethod {
+    CASH("Efectivo", 20, 0.20f),
+    CHECK("Cheque", 15, 0.15f),
+    DEBITCARD("Tarjeta Débito", 17, 0.17f),
+    CREDITCARD("Tarjeta Crédito", 10, 0.10f),
+    CREDIT("Crédito", 0, 0.0f);
+    
+    private final String name;
+    private final int discountPercent;
+    private final float discount;
+
+    PaymentMethod(final String name, int discountPercent, float discount) {
+            this.name = name;
+            this.discountPercent = discountPercent;
+            this.discount = discount;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getDiscountPercent() {
+        return discountPercent;
+    }
+
+    public float getDiscount() {
+        return discount;
+    }
+    
+    public static PaymentMethod getByName(String name) {
+        try {
+            return PaymentMethod.valueOf(name);
+        } catch (Exception ex) {
+            return null;
+        }
+    }
+}
